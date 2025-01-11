ad6395fbea2e1da98557bdfc2cc1e89514f72d04
123342dsf <167676702+123342dsf@users.noreply.github.com>
2025-01-11 19:34:40 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
+
+export default {
+  server:{
+    port:5500,
+    // host:'192.168.43.208',
+    // open:false,
+    // https: false,
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+};
